0190b43e617c29517e5c0a2685e3755e9efe0596
PrabhjotS09 <97964173+PrabhjotS09@users.noreply.github.com>
2022-02-01 15:35:55 -0600

    Update CardDealer.java

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -14,6 +14,7 @@ public class CardDealer
     public static void main(String[] args)
     {
         card obj= new card("abc",1)
+        obj.setSuit("hearts");  
                 /*Modifier- Prabhjot Singh
                 Student number- 991633748*/
         Card nineClubs = new Card("Clbs", 9);
